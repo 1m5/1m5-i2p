34b49d924b1958577aae157165a86018b8aa8297
objectorange <objectorange@protonmail.com>
2018-11-15 17:02:39 -0500

    change to verify connection with seed node

--- src/main/java/io/onemfive/i2p/tasks/TaskRunner.java
+++ src/main/java/io/onemfive/i2p/tasks/TaskRunner.java
@@ -3,6 +3,7 @@ package io.onemfive.i2p.tasks;
 import io.onemfive.core.util.AppThread;
 import io.onemfive.data.DID;
 import io.onemfive.data.Envelope;
+import io.onemfive.data.Peer;
 import io.onemfive.data.util.DLC;
 import io.onemfive.i2p.I2PSensor;
 import io.onemfive.sensors.SensorRequest;
@@ -27,14 +28,24 @@ public class TaskRunner extends AppThread {
 
     private Status status = Status.Shutdown;
     private I2PSensor sensor;
+    private boolean isSeed = false;
+    private DID seedDID;
     private DID localDID;
     private Properties properties;
     private Map<String,Long> checks = new HashMap<>();
 
-    public TaskRunner(I2PSensor sensor, DID localDID, Properties properties) {
+    public TaskRunner(I2PSensor sensor, DID localDID, DID seedDID, Properties properties) {
         this.sensor = sensor;
         this.localDID = localDID;
+        this.seedDID = seedDID;
         this.properties = properties;
+        isSeed = (localDID != null
+                && seedDID != null
+                && localDID.getPeer(Peer.NETWORK_I2P) != null
+                && seedDID.getPeer(Peer.NETWORK_I2P) != null
+                && localDID.getPeer(Peer.NETWORK_I2P).getAddress() != null
+                && seedDID.getPeer(Peer.NETWORK_I2P).getAddress() != null
+                && localDID.getPeer(Peer.NETWORK_I2P).getAddress().equals(seedDID.getPeer(Peer.NETWORK_I2P).getAddress()));
     }
 
     @Override
@@ -42,17 +53,23 @@ public class TaskRunner extends AppThread {
         status = Status.Running;
         LOG.info("I2PSensor Task Runner running...");
         while(status == Status.Running) {
-            // Now send a message to itself to verify it's online
-            String connectedVerifier = "1M5:" + new SecureRandom().nextLong();
-            checks.put(connectedVerifier, System.currentTimeMillis());
-            LOG.info("I2P Router Status: "+sensor.getRouterStatus().name()+"; Sending: "+connectedVerifier);
-            SensorRequest r = new SensorRequest();
-            r.to = localDID;
-            r.from = localDID;
-            Envelope e = Envelope.documentFactory();
-            DLC.addData(SensorRequest.class, r, e);
-            DLC.addContent(connectedVerifier, e);
-            sensor.send(e);
+            LOG.info("I2P Router Status: " + sensor.getRouterStatus().name());
+            // Now send a message to the seed node to verify it's online
+            long now = System.currentTimeMillis();
+            if(!isSeed) {
+                String connectedVerifier = "1M5-ConnectionVerify:" + now;
+                checks.put(connectedVerifier, now);
+                LOG.info("Sending: " + connectedVerifier);
+                LOG.info("  To: "+seedDID.getPeer(Peer.NETWORK_I2P).getAddress());
+                LOG.info("  From: "+localDID.getPeer(Peer.NETWORK_I2P).getAddress());
+                SensorRequest r = new SensorRequest();
+                r.to = seedDID;
+                r.from = localDID;
+                Envelope e = Envelope.documentFactory();
+                DLC.addData(SensorRequest.class, r, e);
+                DLC.addContent(connectedVerifier, e);
+                sensor.send(e);
+            }
             try {
                 synchronized (this) {
                     this.wait(timeBetweenRunsMinutes * 60 * 1000);
@@ -70,6 +87,7 @@ public class TaskRunner extends AppThread {
             LOG.info("Message received ("+code+") not a connection verifier. Ignoring.");
         else {
             LOG.info("Received connection verifier " + code + " response in " + ((System.currentTimeMillis() - begin)/1000) + " seconds.");
+            checks.remove(code);
         }
     }
 
